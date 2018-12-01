643c07868b5d4c7e3e2f816ce288eabe6eb66668
Fitze, Larissa <larissa.fitze@unisg.ch>
2018-12-01 14:43:00 +0100

    Create Login Page

--- LAFI.DMS/Views/LoginWindow.xaml.cs
+++ LAFI.DMS/Views/LoginWindow.xaml.cs
@@ -10,19 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LAFI.DMS
+namespace LAFI.DMS.Views
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window
-  {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
     {
-      InitializeComponent();
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
     }
-  }
 }
